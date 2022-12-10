85906dc510e4cd72443052ff934ecafe27645fa4
Tesfaye <adugnatesfaye888@gmail.com>
2022-12-10 14:43:41 +0300

    initial commit

---
+++ src/data.js
@@ -0,0 +1,68 @@
+export const projects = [
+  {
+    title: "BertuCAN",
+    subtitle: "Oct 2022 - Jan 2023",
+    description:
+      "Bertucan is A platform that tracks a period of women and gives suggestion based on the given information  Implemented Ethiopian Calendar and integrated it with the app. Used GetX State management.",
+    image: "./home_page.jpg",
+    link: "",
+  },
+  {
+    title: "RateEat",
+    subtitle: "Jul 2022 - Sep 2022",
+    description:
+      "RateEat is A platform that allows users to give and consume item specific reviews and show growth points of businesses. Implemented 70% of the screens from Figma designs. Used BLoC State management and CLEAN architecture while implementing the review feature of RateEat",
+    image: "./rateeat.jpg",
+    link: "https://rateeat.app/",
+  },
+  {
+    title: "Erdata",
+    subtitle: "Oct 2021 - Jul 2022",
+    description:
+      "Erdata is web and mobile app intended to facilitate humanitarian support to street children by widening the exposure for potential aid. Implemented the website using Html, CSS and Bootstrap. Used Spring Boot to implement the backend. Developed the mobile app using Flutter.",
+    image: "./helping.jpg",
+    link: "https://github.com/TesfayeAdugna/Erdata-App",
+  },
+  {
+    title: "Agenagn",
+    subtitle: "Oct 2021 - Jan 2022",
+    description:
+      "Agenagn is an E-commerce website that allows people to buy and sell used items. Worked as a full stack developer in a team of 5. Used Html, CSS, Bootstrap and spring boot to  build the website.",
+    image: "./agenagn.jpg",
+    link: "https://github.com/TesfayeAdugna/Agenagn",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Python",
+  "Flutter",
+  "JavaScript",
+  "Dart",
+  "Spring Boot",
+  "Django",
+  "Figma",
+  "BootStrap",
+  "MySQL",
+  "Angular",
+  "React",
+  "Git",
+  "Trello",
+  "Agile Methodology"
+];
